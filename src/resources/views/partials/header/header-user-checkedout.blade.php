8c0f05d763b2abd86e3a80404d2645309e431cd6
misa <mmichiura33@gmail.com>
2025-05-20 21:13:56 +0900

    退勤時のヘッダーを修正

--- src/resources/views/partials/header/header-user-checkedout.blade.php
+++ src/resources/views/partials/header/header-user-checkedout.blade.php
@@ -1,6 +1,6 @@
 {{-- 一般ユーザー用退勤時ヘッダー --}}
 <nav class="header__links">
-    <a class="header__link" href="{{ route('attendance.list') }}">今月の勤怠一覧</a>
+    <a class="header__link" href="{{ route('attendance.list') }}">今月の出勤一覧</a>
     <a class="header__link" href="{{ route('stamp_correction_request.list') }}">申請一覧</a>
     <form action="{{ route('logout') }}" method="POST">
         @csrf
